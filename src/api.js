5dd7292612da9f8598e3a434d132a60a4c26a7d2
Chinh Vu <vucongchinh321@gmail.com>
2022-07-09 01:39:38 +0700

    done

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+		'X-RapidAPI-Key': '9623502654msh1454cac158f05a9p14db10jsn3313649db999',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+export const geoApiURL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3fcb7c90a70d418d59ffc69c37cbc698";
